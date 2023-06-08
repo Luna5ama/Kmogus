aaa08d65fffc78eb045228efab7184262461e4d8
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-06-08 02:54:20 -0400

    Nested struct support
    (cherry picked from commit e6bfa5a1a7563f31fc8eb2fb030a93cd5880ee5d)

--- struct-api/src/main/kotlin/dev/luna5ama/kmogus/struct/Api.kt
+++ struct-api/src/main/kotlin/dev/luna5ama/kmogus/struct/Api.kt
@@ -2,7 +2,7 @@ package dev.luna5ama.kmogus.struct
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
-annotation class Struct(val sizeAlignment: Long = 4L, val fieldAlignment: Boolean = false)
+annotation class Struct(val sizeAlignment: Long = 4L, val fieldAlignment: Boolean = false, val size: Long = -1)
 
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.SOURCE)
